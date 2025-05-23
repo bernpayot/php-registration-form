1f6fb8fabd64ad342723ca767b5e72f41ea96b9b
bernpayot <payotao@students.nu-dasma.edu.ph>
2025-05-23 22:22:20 +0800

    Commit final state

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TestPHP | <?php echo $pagetitle ?></title>
+    <title>TestPHP | <?php echo $pagetitle; ?></title>
 </head>
 <body>
     <div class="login">
